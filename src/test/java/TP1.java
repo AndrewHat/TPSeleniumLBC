7503fb96c0f47b7b2755b1cf5cd343762a14357d
Andrew <hatoumandrew@hotmail.com>
2021-05-26 14:17:27 +0200

    First Test of Ebay: Place first element in cart

--- src/test/java/TP1.java
+++ src/test/java/TP1.java
@@ -1,12 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TP1 {
@@ -24,28 +29,44 @@ public class TP1 {
     }
 
     @BeforeMethod
-    public void openLBC() {
+    public void openEbay() {
         driver = new ChromeDriver(options);
-        driver.get("https://www.leboncoin.fr/");
+        driver.get("https://www.ebay.fr/");
         driver.manage().window().maximize();
-        driver.findElement(By.id("didomi-notice-agree-button")).click();
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.elementToBeClickable(By.id("gdpr-banner-accept"))).click();
     }
 
     @AfterMethod
-    public void closeLBC() {
+    public void closeEbay() {
         driver.quit();
     }
 
     @Test
-    public void FirstResultSearch() {
+    public void FirstResultAddCart() {
         // Arrange
+        String cssVacanceSport = "#gh-sbc tr td+td ul li";
+        String cssFirstElem = ".srp-results.srp-grid > li:first-child";
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
         // Act
-        driver.findElement(By.id("text-categories-desktop")).click();
-        driver.findElement(By.id("cta-categories_category_emploi-desktop")).click();
-        driver.findElement(By.id("cta-categories_category_offres_d_emploi-desktop-child")).click();
-        driver.findElement(By.id("input-keywords-desktop")).sendKeys("QA");
+        driver.findElement(By.id("gh-shop-a")).click();
+        List<WebElement> l = driver.findElements(By.cssSelector(cssVacanceSport));
+        l.get(2).findElement(By.cssSelector("a")).click();
+
+        driver.findElement(By.cssSelector(cssFirstElem)).click();
+
+        By elemCSS = By.id("isCartBtn_btn");
+        wait.until(ExpectedConditions.elementToBeClickable(elemCSS)).click();
+        WebElement cartUpdate = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("gh-cart-n")));
+        String cartNumber = cartUpdate.getText();
+        
+        driver.findElement(By.cssSelector(".gh-cart-icon")).click();
+        WebElement we = driver.findElement(By.cssSelector("[data-test-id=\"cart-bucket\"] div.image-display"));
+        Boolean isNotEmptyCart = we != null;
 
         // Assert
+        Assert.assertTrue(cartNumber.equals("1"), "Error number in cart = "+cartNumber);
+        Assert.assertTrue(isNotEmptyCart , "Cart is empty");
     }
 }
